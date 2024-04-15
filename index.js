ded21806a0abd619942efdc13893b83d7456535e
Mitesh-Amin01 <aminmiteshkumar.1484@gmail.com>
2024-04-15 10:32:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send("Twitter App")
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login Now</h1>')
+})
+app.get('/enjoy',(req,res)=>{
+    res.send('<h1>You Are Enjoy Your Day.</h1>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
